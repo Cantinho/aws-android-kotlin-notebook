22467b9bdc5d595b3fcbf32f05d84b68288f6721
samirtf <samircc20092@gmail.com>
2020-01-21 20:31:28 -0300

    add simple login activity

---
+++ cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package br.com.cantinho.awsauthcognito.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
---
+++ cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package br.com.cantinho.awsauthcognito.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
