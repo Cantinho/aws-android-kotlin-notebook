7c5c991b46fccdc1c5be457cbb81e4a7640ce535
samirtf <samircc20092@gmail.com>
2020-01-21 22:06:46 -0300

    add cognito user pool in koin module

--- cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/modules/KoinModule.kt
+++ cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/modules/KoinModule.kt
@@ -3,10 +3,13 @@ package br.com.cantinho.awsauthcognito.modules
 import android.app.Application
 import br.com.cantinho.awsauthcognito.CantinhoApplication
 import br.com.cantinho.awsauthcognito.aws.auth.AwsConfigurationProvider
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool
 import org.koin.android.BuildConfig
 import org.koin.dsl.module
 
 val applicationModule = module {
     single<Application> { CantinhoApplication() }
     single { (application: Application) -> AwsConfigurationProvider(application.applicationContext, BuildConfig.BUILD_TYPE) }
+    single { (application: Application, awsConfigurationProvider: AwsConfigurationProvider) -> CognitoUserPool(application.applicationContext, awsConfigurationProvider.awsConfiguration) }
+
 }
