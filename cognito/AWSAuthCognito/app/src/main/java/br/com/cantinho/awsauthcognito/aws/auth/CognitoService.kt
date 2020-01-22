b8f67a5e5b0f0c5f556b5a5dac71ed2d4e3b5609
samirtf <samircc20092@gmail.com>
2020-01-21 22:31:22 -0300

    add cognito service initialization

--- cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/aws/auth/CognitoService.kt
+++ cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/aws/auth/CognitoService.kt
@@ -2,10 +2,29 @@ package br.com.cantinho.awsauthcognito.aws.auth
 
 import com.amazonaws.auth.CognitoCredentialsProvider
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.lang.StringBuilder
 
-class CognitoService(val userPool: CognitoUserPool, val credentialsProvider: CognitoCredentialsProvider) {
+class CognitoService(val awsConfigurationProvider: AwsConfigurationProvider, val userPool: CognitoUserPool, val credentialsProvider: CognitoCredentialsProvider) {
 
-
+    val identityUserPoolAppClientId: String
+    val cognitoProviderUrl: String
+    init {
+        try {
+            val optJsonObject = awsConfigurationProvider.awsConfiguration.optJsonObject("CognitoUserPool")
+            identityUserPoolAppClientId = optJsonObject.getString("AppClientId")
+            val region = optJsonObject.get("Region")
+            val userPoolId = optJsonObject.get("PoolId")
+            cognitoProviderUrl = StringBuilder()
+                .append("cognito-idp.")
+                .append(region)
+                .append(".amazonaws.com/")
+                .append(userPoolId).toString()
+        } catch (exc: Exception) {
+            throw IllegalArgumentException("Unable to initialize ${CognitoService::class.qualifiedName}.Verify your aws configuration file.", exc)
+        }
+    }
 
 
 }
