f2590fddd9183e2964efd51d0cbbfb0cbc56daf2
samirtf <samircc20092@gmail.com>
2020-01-21 22:11:13 -0300

    add cognito credentials provider in koin module

--- cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/modules/KoinModule.kt
+++ cognito/AWSAuthCognito/app/src/main/java/br/com/cantinho/awsauthcognito/modules/KoinModule.kt
@@ -3,6 +3,8 @@ package br.com.cantinho.awsauthcognito.modules
 import android.app.Application
 import br.com.cantinho.awsauthcognito.CantinhoApplication
 import br.com.cantinho.awsauthcognito.aws.auth.AwsConfigurationProvider
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.auth.CognitoCredentialsProvider
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool
 import org.koin.android.BuildConfig
 import org.koin.dsl.module
@@ -11,5 +13,6 @@ val applicationModule = module {
     single<Application> { CantinhoApplication() }
     single { (application: Application) -> AwsConfigurationProvider(application.applicationContext, BuildConfig.BUILD_TYPE) }
     single { (application: Application, awsConfigurationProvider: AwsConfigurationProvider) -> CognitoUserPool(application.applicationContext, awsConfigurationProvider.awsConfiguration) }
+    single<CognitoCredentialsProvider> { (application: Application, awsConfigurationProvider: AwsConfigurationProvider) -> CognitoCachingCredentialsProvider(application.applicationContext, awsConfigurationProvider.awsConfiguration) }
 
 }
